604699184bb9ebc3b04d4307a4968c472d775d15
HyeonJin__ <63301908+Hyeonjin-ee@users.noreply.github.com>
2022-07-04 22:42:32 +0900

    [220704] 단계별 : 반복문

---
+++ 반복문/BJO_10950.py
@@ -0,0 +1,8 @@
+## A + B - 3
+import sys
+
+T = int(input())
+
+for i in range(1, T+1) :
+    A, B = map(int, sys.stdin.readline().split())
+    print(A + B)
---
+++ 반복문/BJO_2739.py
@@ -0,0 +1,8 @@
+## 구구단 출력하기 
+
+n = int(input())
+
+for i in range(1,10) :
+    print(f"{n} * {i} = {n*i}")
+
+
